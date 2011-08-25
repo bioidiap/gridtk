1f93d7aabf6815cc9f214b2c4208f42228c31bae
Andre Anjos <andre.anjos@idiap.ch>
2011-08-25 16:20:08 +0200

    Sets up gridtk

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+# Andre Anjos <andre.anjos@idiap.ch>
+# Thu 25 Aug 2011 16:17:15 CEST
+
+dir=`dirname ${BASH_SOURCE[0]}`
+export PATH=${dir}:${PATH}
+if [ -z "${PYTHONPATH}" ]; then
+  export PYTHONPATH=${dir}
+else
+  export PYTHONPATH=${dir}:${PYTHONPATH}
+fi
