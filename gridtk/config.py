bd24d9985d03e6d2c9a965209aa4b1decbc54927
Andre Anjos <andre.anjos@idiap.ch>
2012-07-13 09:31:31 +0200

    Memory setting option + version info (forgot file)

---
+++ gridtk/config.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+# Andre Anjos <andre.anjos@idiap.ch>
+# Fri 13 Jul 2012 08:49:43 CEST 
+
+"""Returns the currently compiled version number"""
+
+__version__ = __import__('pkg_resources').require('gridtk')[0].version
