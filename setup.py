9b3a72c5a76de97c868cff0d70f6878a96e4cb85
Andre Anjos <andre.anjos@idiap.ch>
2012-09-26 12:10:07 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gridtk',
-    version='0.2.1',
+    version='0.3.0',
     description='SGE Grid Submission and Monitoring Tools for Idiap',
 
     url='https://github.com/idiap/gridtk',
