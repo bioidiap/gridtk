9ccefc693468ba5de7585255d35a64944ac1a29e
Andre Anjos <andre.anjos@idiap.ch>
2016-08-16 16:28:28 +0200

    [gridtk] Add get_config() call

--- gridtk/__init__.py
+++ gridtk/__init__.py
@@ -5,3 +5,14 @@ from . import local
 from . import sge
 from . import easy
 from . import tests
+
+
+def get_config():
+  """Returns a string containing the configuration information.
+  """
+  import bob.extension
+  return bob.extension.get_config(__name__)
+
+
+# gets sphinx autodoc done right - don't remove it
+__all__ = [_ for _ in dir() if not _.startswith('_')]
